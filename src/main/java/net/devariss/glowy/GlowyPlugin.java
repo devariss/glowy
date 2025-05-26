d0c472ab27f1560171ef200214b30bc7d47a7d28
var <132378006+DeVariss@users.noreply.github.com>
2025-05-26 12:34:56 -0400

    Initial Project Creation

---
+++ src/main/java/net/devariss/glowy/GlowyPlugin.java
@@ -0,0 +1,17 @@
+package net.devariss.glowy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GlowyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
