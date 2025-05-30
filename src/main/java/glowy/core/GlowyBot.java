b06405b615a5bca4452ee5db0f5943bb7298e7bd
var <132378006+DeVariss@users.noreply.github.com>
2025-05-30 17:28:01 -0400

    Moved config logic to sep record

--- src/main/java/glowy/core/GlowyBot.java
+++ src/main/java/glowy/core/GlowyBot.java
@@ -8,35 +8,44 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.security.InvalidParameterException;
 
 public record GlowyBot(
     DiscordClient client,
     Snowflake chatChannelId
 ) {
-    public static GlowyBot config(Path path) {
-        record BotConfig(
-            String token,
-            String chatChannelId
-        ){}
-        var gson = new GsonBuilder().setPrettyPrinting().create();
+    public static GlowyBot of(Path path) {
         try {
-            if (Files.exists(path)) {
-                var config = gson.fromJson(Files.readString(path), BotConfig.class);
-                return new GlowyBot(
-                    DiscordClient.create(config.token),
-                    Snowflake.of(config.chatChannelId)
-                );
-            }
-            Files.createDirectories(path.getParent());
-            Files.writeString(
-                path,
-                gson.toJson(new BotConfig("", "")),
-                StandardOpenOption.CREATE_NEW
+            var config = BotConfig.from(path);
+            return new GlowyBot(
+                DiscordClient.create(config.token),
+                Snowflake.of(config.chatChannelId)
             );
+        }
+        catch (InvalidParameterException e) {
             throw new IllegalStateException("First startup, please configure at path '%s'".formatted(path));
         }
         catch (IOException e) {
             throw new IllegalStateException("Failed to read config at path '%s' on enabled with exception:\n%s".formatted(path, e));
+        }
+    }
+
+    private record BotConfig(
+        String token,
+        String chatChannelId
+    ) {
+        public static BotConfig from(Path path) throws IOException, InvalidParameterException {
+            var gson = new GsonBuilder().setPrettyPrinting().create();
+            if (Files.exists(path)) {
+                return gson.fromJson(Files.readString(path), BotConfig.class);
+            }
+            Files.createDirectories(path.getParent());
+            Files.writeString(path, gson.toJson(BotConfig.blank()), StandardOpenOption.CREATE_NEW);
+            throw new InvalidParameterException();
+        }
+
+        private static BotConfig blank() {
+            return new BotConfig("", "");
         }
     }
 }
