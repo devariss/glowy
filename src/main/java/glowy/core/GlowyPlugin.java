1e84c58a2d2fc7131f1e7a246b12971107126bf7
var <132378006+DeVariss@users.noreply.github.com>
2025-05-30 17:28:53 -0400

    Forwards player chat to configged text channel

--- src/main/java/glowy/core/GlowyPlugin.java
+++ src/main/java/glowy/core/GlowyPlugin.java
@@ -1,31 +1,70 @@
 package glowy.core;
 
+import com.google.common.primitives.Bytes;
+import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Webhook;
 import discord4j.core.object.entity.channel.TextChannel;
+import discord4j.core.spec.WebhookCreateSpec;
+import discord4j.core.spec.WebhookEditSpec;
+import discord4j.core.spec.WebhookExecuteSpec;
+import discord4j.discordjson.possible.Possible;
+import discord4j.rest.util.Image;
+import glowy.util.images.Size;
+import glowy.util.minecraft.Players;
 import glowy.util.plugins.ReactivePlugin;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.function.BiFunction;
 
 public final class GlowyPlugin extends ReactivePlugin {
+    private final GlowyBot BOT = GlowyBot.of(Path.of("%s/config.json".formatted(getDataFolder())));
+
     @Override
     public Mono<Void> onInit() {
-        var bot = GlowyBot.config(Path.of("%s/config.json".formatted(getDataFolder())));
         return Mono.when(
-            broadcastDiscordChat(bot),
-            forwardPlayerChat(bot)
+            forwardPlayerChat(),
+            broadcastDiscordChat()
         );
     }
 
-    // implement
-    private Mono<Void> forwardPlayerChat(GlowyBot bot) {
-        return Mono.empty();
+    //TODO runs on game logic thread, need to move so reading avatar doesn't run on game thread.
+    private Mono<Void> forwardPlayerChat() {
+        BiFunction<Webhook, AsyncPlayerChatEvent, Mono<Webhook>> editHook = (hook, event) -> {
+            String playerName = event.getPlayer().getName();
+            return hook.getName()
+                .filter(name -> name.equals(playerName))
+                .map(name -> Mono.just(hook))
+                .orElseGet(() -> {
+                        var hookEdit = WebhookEditSpec.builder().name(playerName);
+                        try {
+                            hookEdit.avatar(Image.ofRaw(Bytes.toArray(Players.getPlayerAvatar(event.getPlayer(), Size.of(512, 512))), Image.Format.PNG));
+                        }
+                        catch (IOException e) {
+                            hookEdit.avatar(Possible.absent());
+                        }
+                        return hook.edit(hookEdit.build());
+                    });
+        };
+        return BOT.client().withGateway(gateway -> gateway.getChannelById(BOT.chatChannelId())
+            .cast(TextChannel.class)
+            .flatMap(channel -> channel.createWebhook(WebhookCreateSpec.builder().name("glowy chat hook").build()))
+            .flatMap(hook -> Mono.just(hook)
+                .doOnCancel(() -> hook.delete().block()))
+            .flatMap(hook -> on(AsyncPlayerChatEvent.class)
+                .flatMap(event -> editHook.apply(hook, event)
+                    .flatMap(__ -> hook.execute(WebhookExecuteSpec.builder()
+                        .content("%s".formatted(event.getMessage()))
+                        .build())))
+            .then()));
     }
 
-    private Mono<Void> broadcastDiscordChat(GlowyBot bot) {
-        return bot.client().withGateway(gateway -> gateway.on(MessageCreateEvent.class)
-            .filter(event -> event.getMessage().getChannelId().equals(bot.chatChannelId()))
+    private Mono<Void> broadcastDiscordChat() {
+        return BOT.client().withGateway(gateway -> gateway.on(MessageCreateEvent.class)
+            .filter(event -> event.getMessage().getChannelId().equals(BOT.chatChannelId()))
             .flatMap(event -> Mono.fromRunnable(() -> event.getMember()
                 .ifPresent(author -> getServer().broadcastMessage("<@%s> %s".formatted(author.getDisplayName(), event.getMessage().getContent()))))));
     }
